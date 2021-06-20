1d9e49db3c873f794377682c24b68f1028772ddc
Andrew <jganabis@outlook.com>
2021-06-20 22:10:37 +0200

    added ChatsSeen ef migrations

---
+++ Data/Migrations/20210620200252_AddedChatsSeenFields.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class AddedChatsSeenFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AnswererSeen",
+                schema: "Topicly",
+                table: "Chats",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CreatorSeen",
+                schema: "Topicly",
+                table: "Chats",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AnswererSeen",
+                schema: "Topicly",
+                table: "Chats");
+
+            migrationBuilder.DropColumn(
+                name: "CreatorSeen",
+                schema: "Topicly",
+                table: "Chats");
+        }
+    }
+}
